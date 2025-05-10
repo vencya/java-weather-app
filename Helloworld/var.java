e7a65124b7492d375f61eb720c303f1abf087f04
vencya <gvencya@gmail.com>
2025-05-10 20:57:03 +0530

    Add files via upload

---
+++ Helloworld/var.java
@@ -0,0 +1,11 @@
+package Helloworld;
+
+public class add {
+    public static void main (String args[]){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.err.println("the sum is: "+c);
+    }
+    
+}
